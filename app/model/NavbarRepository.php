217c9c1cec9ffa23902983bfecde267e2491bdf6
JDC <jdc@2ne1.cz>
2014-02-17 23:25:37 +0100

    Added NavBar controls (issue #3) Added base navbar control PlaylistBar

---
+++ app/model/NavbarRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Copyright (c) 2014, JDCofficial
+ * All rights reserved.
+ *
+ * License information you can find in LICENSE.md
+ */
+namespace App\Model;
+/**
+ * Description of NavbarRepository
+ *
+ * @author JDC
+ */
+class NavbarRepository extends Repository {
+	//put your code here
+}
--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -3,6 +3,7 @@
 namespace App\Presenters;
 
 use Nette,
+	App\Controls\Navbar,
 	App\Model;
 
 
@@ -11,8 +12,27 @@ use Nette,
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
+	/** @var \FactoryCreator @inject */
+	public $factory;
+	
+	/** @var \App\Model\NavbarRepository @inject */
+	public $navbar;
+
 	protected function viewPage($page) {
 		$contentDir = "/../../../content";		
 		$this->setView($contentDir . "/$page");
+	}
+	
+	protected function createComponentNavbar() {
+		$navbar = new Navbar();
+		
+		foreach ($this->navbar->findAll() as $nav) {
+			$control = $this->factory->create($nav->factory);
+			$navbar->addControl(Navbar::SIDE_LEFT, $control, "nav_".$nav->id);
+			if ($nav->config)
+				$control->setup($nav->config);
+		}
+		
+		return $navbar;
 	}
 }
