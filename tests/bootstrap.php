4a6af9d80630c35917f307442182dca1e0f0fe9d
JDC <jdc@2ne1.cz>
2014-02-18 16:25:23 +0100

    Added test neon config file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,5 +21,5 @@ $configurator->createRobotLoader()
 	->register();
 
 $configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-//$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
+$configurator->addConfig(__DIR__ . '/config.test.neon');
 return $configurator->createContainer();
