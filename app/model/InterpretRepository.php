cd4f6c0ddbbe75cf3f404608bf5e1b26a328bc32
JDC <jdc@2ne1.cz>
2014-02-20 16:32:50 +0100

    Model: Added Interpret repository

---
+++ app/model/InterpretRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+/* Copyright (c) 2014, JDCofficial
+ * All rights reserved.
+ *
+ * License information you can find in LICENSE.md
+ */
+namespace App\Model;
+use Nette\Database\Table\ActiveRow;
+
+/**
+ * Description of InterpretRepository
+ *
+ * @author JDC
+ */
+class InterpretRepository extends Repository {
+	
+	public function getByName($name, $skipalias = true) {
+		$r = $this->getTable()->where("nazev",$name)->fetch();
+		
+		if ($r && $skipalias) 
+			return $this->follow($r);	
+		return $r;
+	}
+	
+	protected function follow(ActiveRow $row) {
+		if ($row->interpret_id)
+			return $this->follow($row->interpret);
+		return $row;
+	}
+	
+}
