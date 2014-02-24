b837c6fdd51a0e434b459b38e05678d8ae5bd5fb
JDC <jdc@2ne1.cz>
2014-02-24 21:20:20 +0100

    matchInterpret: Added styles
    Optimalize for bootstrap 3

--- www/js/main.js
+++ www/js/main.js
@@ -1,20 +1,23 @@
 
 function matchInterpret(json, selector) {
-	$(selector).removeClass("sg-mi sg-mi-matched sg-mi-alias sg-mi-youmean"); //reset
+	$(selector).removeClass("sg-mi sg-mi-matched sg-mi-alias sg-mi-youmean text-success text-info text-danger text-primary text-warning"); //reset
+	$(selector).parent().removeClass("has-success has-warning has-error"); //reset parent
 	$(selector).addClass("sg-mi"); //base class
 	if (json.match) {
 		if (json.distance === 0) {
 			if (json.alias === false) {
-				$(selector).addClass("sg-mi-matched");
+				$(selector).addClass("text-success sg-mi-matched");
+				$(selector).parent().addClass("has-success");
 				$(selector).html("Interpret <strong>"+json.matched+"</strong> je v našem registru");
 			}
 			else {
-				$(selector).addClass("sg-mi-alias");
+				$(selector).addClass("text-info sg-mi-alias");
 				$(selector).html("Interpret <strong>"+json.matched+"</strong> je aliasem pro <strong>"+json.alias+"</strong>");
 			}
 		}
 		else {
-			$(selector).addClass("sg-mi-youmean");
+			$(selector).addClass("text-warning sg-mi-youmean");
+			$(selector).parent().addClass("has-warning");
 			$(selector).html("Měli jste na mysli <strong>"+json.matched+"</strong>?");
 		}
 	}
