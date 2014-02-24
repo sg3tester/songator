7bc1c0e0ceade09cf7ebe57eea46f20bcdd3ada8
JDC <jdc@2ne1.cz>
2014-02-24 20:35:00 +0100

    matchInterpret: Added classes

--- www/js/main.js
+++ www/js/main.js
@@ -1,20 +1,25 @@
 
 function matchInterpret(json, selector) {
+	$(selector).removeClass("sg-mi sg-mi-matched sg-mi-alias sg-mi-youmean"); //reset
+	$(selector).addClass("sg-mi"); //base class
 	if (json.match) {
 		if (json.distance === 0) {
 			if (json.alias === false) {
+				$(selector).addClass("sg-matched");
 				$(selector).html("Interpret <strong>"+json.matched+"</strong> je v našem registru");
 			}
 			else {
+				$(selector).addClass("sg-alias");
 				$(selector).html("Interpret <strong>"+json.matched+"</strong> je aliasem pro <strong>"+json.alias+"</strong>");
 			}
 		}
 		else {
+			$(selector).addClass("sg-youmean");
 			$(selector).html("Měli jste na mysli <strong>"+json.matched+"</strong>?");
 		}
 	}
 	else {
-		$(selector).html(null);
+		$(selector).html(null); //hide
 	}
 }
 
