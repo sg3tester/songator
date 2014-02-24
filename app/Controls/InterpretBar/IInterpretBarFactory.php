35bd893e533c8dc65f92a9003cd8695e64c6e3f2
JDC <jdc@2ne1.cz>
2014-02-24 14:39:28 +0100

    Added InterpretBar

---
+++ app/Controls/InterpretBar/IInterpretBarFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Copyright (c) 2014, JDCofficial
+ * All rights reserved.
+ *
+ * License information you can find in LICENSE.md
+ */
+namespace App\Controls;
+/**
+ *
+ * @author JDC
+ */
+interface IInterpretBarFactory {
+	
+	/**
+	 * @return \App\Controls\InterpretBar
+	 */
+	public function create();
+}
