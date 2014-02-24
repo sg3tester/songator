0a1185daade84fc590caf050c1f3572c635d6c79
JDC <jdc@2ne1.cz>
2014-02-24 20:15:11 +0100

    Added base matchInterpret parser

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,9 @@
+
+function matchInterpret(json) {
+	console.info(json.matching);
+	return "lol";
+}
+
 $(document).ready(function(){
 	$(".reason").click(function(){
 	    $("#reason").val($(this).text());
