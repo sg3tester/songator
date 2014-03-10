5a65ca53b5f554e51e7dfe27df0247c71de9c246
JDC <jdc@2ne1.cz>
2014-03-10 12:20:28 +0100

    AddSong: Added song matching

--- www/js/main.js
+++ www/js/main.js
@@ -28,6 +28,45 @@ function matchInterpret(json, selector) {
 	$(".match").click(setMatch);
 }
 
+function rndColor() {
+	var n = Math.floor((Math.random()*5)+1); 
+	switch (n) {
+		case 1:
+			return "text-primary";
+		case 2:
+			return "text-success";
+		case 3:
+			return "text-info";
+		case 4:
+			return "text-warning";
+		case 5:
+			return "text-danger";
+	}
+}
+
+function matchSong(json, selector) {
+	$(selector).removeClass("sg-mi sg-mi-matched sg-mi-alias sg-mi-youmean text-success text-info text-danger text-primary text-warning"); //reset
+	$(selector).parent().removeClass("has-success has-warning has-error has-feedback"); //reset parent
+	$(selector).addClass("sg-mi"); //base class
+	if (json.match) {
+		if (json.matched && json.matching.interpret) {
+				$(selector).addClass("text-danger sg-mi-matched");
+				$(selector).parent().addClass("has-error");
+				$(selector).html("<i class='glyphicon glyphicon-warning-sign'></i> Song <strong>"+json.matched.interpret+" - "+json.matched.name+"</strong> je již v playlistu");
+			}
+			
+		else {
+			$(selector).html('<strong><ul id="ms-list" class="list-inline"></ul></strong>');
+			json.similar.forEach(function(entry){
+				$("#ms-list").append('<li class="'+rndColor()+'">'+entry.interpret+" - "+entry.name+"</li>");
+			});
+		}
+	}
+	else {
+		$(selector).html(null); //hide
+	}
+}
+
 function setMatch() {
 	$("#interpret").val($(this).text());
 	$("#interpret").change();
