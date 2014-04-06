f56d5d5da95cda9cbde80d858b6fea5599c83c7d
JDC <jdc@2ne1.cz>
2014-04-06 18:33:33 +0200

    Admin: Inital commit

---
+++ app/AdminModule/presenters/BasePresenter.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Copyright (c) 2014, JDCofficial
+ * All rights reserved.
+ *
+ * License information you can find in LICENSE.md
+ */
+namespace App\AdminModule\Presenters;
+/**
+ * Description of BasePresenter
+ *
+ * @author JDC
+ */
+abstract class BasePresenter extends \Nette\Application\UI\Presenter {
+	//put your code here
+}
---
+++ app/AdminModule/presenters/DashboardPresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Copyright (c) 2014, JDCofficial
+ * All rights reserved.
+ *
+ * License information you can find in LICENSE.md
+ */
+namespace App\AdminModule\Presenters;
+/**
+ * Description of DashboardPresenter
+ *
+ * @author JDC
+ */
+class DashboardPresenter extends BasePresenter {
+	
+	/** @var App\Model\SongRepository @inject */
+	public $songy;
+	
+}
