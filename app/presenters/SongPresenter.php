7f8fd5025d6adfb14529281045a49a6432c8418e
JDC <jdc@2ne1.cz>
2014-04-12 10:14:20 +0200

    SongView: Added backlink buttons

--- app/presenters/SongPresenter.php
+++ app/presenters/SongPresenter.php
@@ -172,7 +172,7 @@ class SongPresenter extends BasePresenter
 			if ($val->private_vzkaz && !$this->user->isAllowed("privateMsg","add"))
 					$form->addError("Nemáte oprávnění označit zprávu pro DJe jako soukromou!");
 			
-			if (($this->settings->get('songator_status', 'enabled') != 'fenabled' || $this->settings->get('songator_wip', false))
+			if (($this->settings->get('songator_status', 'enabled') != 'enabled' || $this->settings->get('songator_wip', false))
 					&& !$this->user->isAllowed("wip","switch"))
 					$form->addError("Omlouváme se, Songator je dočasně vypnut. Nelze přidat song.");
 		};
