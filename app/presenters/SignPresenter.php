1917ef8ccc169ee2a43900ce6b202d72a1126e8e
JDC <jdc@2ne1.cz>
2014-09-11 20:17:16 +0200

    Extended user session time

--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -91,7 +91,7 @@ class SignPresenter extends BasePresenter
 		if ($values->remember) {
 			$this->getUser()->setExpiration('14 days', FALSE);
 		} else {
-			$this->getUser()->setExpiration('20 minutes', TRUE);
+			$this->getUser()->setExpiration('24 hours', TRUE);
 		}
 
 		try {
