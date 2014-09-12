e6cf95dac9b4ed8c1315c797dcdb8ee518ea3739
JDC <jdc@2ne1.cz>
2014-09-12 15:53:50 +0200

    Prepared profile settings as under construction

--- app/presenters/ProfilePresenter.php
+++ app/presenters/ProfilePresenter.php
@@ -6,20 +6,22 @@ use Nette,
 	Nette\Application\UI\Form,
 	App\Model;
 
-
 /**
  * Homepage presenter.
  */
-class ProfilePresenter extends BasePresenter
-{
+class ProfilePresenter extends BasePresenter {
 
 	/** @var \App\UserManager @inject */
 	public $usrmgr;
-	
+
+	public function renderSettings() {
+		$this->setView("default"); //temporarily, under construction
+	}
+
 	public function actionCreate() {
 		if (!$this->user->isLoggedIn() || !$this->usrmgr->getUser($this->user->id)->first_login || !$this->checkPermissions("ucp", "save"))
 			$this->redirect("homepage:");
-		
+
 		if ($this->twitter->isTwitterUser()) {
 			$info = $this->twitter->verifyCredentials();
 			$defaults = array(
@@ -30,46 +32,45 @@ class ProfilePresenter extends BasePresenter
 			if (count($info->entities->url->urls)) {
 				$defaults["www"] = $info->entities->url->urls[0]->expanded_url;
 			}
-		}
-		else {
+		} else {
 			$identity = $this->user->getIdentity();
 			$defaults = array(
 				"realname" => $identity->username
 			);
 		}
-		
+
 		$this["profileCreate"]->setDefaults($defaults);
 	}
-	
+
 	protected function createComponentProfileCreate() {
 		$form = new Form();
-		
+
 		$form->addText("realname");
 		$form->addText("twitter_acc");
 		$form->addText("www");
 		$form->addTextArea("about");
-		
+
 		$form->addSubmit("create");
-		
+
 		$form->onSuccess[] = $this->profileCreateSuccess;
-		
+
 		return $form;
 	}
-	
+
 	public function profileCreateSuccess(Form $form) {
 		$val = $form->getValues(true);
-		
+
 		if (!$this->checkPermissions("ucp", "save"))
 			$this->redirect("homepage:");
-		
+
 		if ($this->twitter->isTwitterUser()) {
 			$info = $this->twitter->verifyCredentials();
 			$val["avatar"] = $info->profile_image_url;
 		}
 		$val["first_login"] = false;
-		
+
 		$this->usrmgr->update($this->user->id, $val);
-		
+
 		$this->redirect("Homepage:");
 	}
 
