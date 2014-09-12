7fd2d149bd3e7e8be4d8d730ee6b02fcdbb7f8bf
JDC <jdc@2ne1.cz>
2014-09-12 11:52:45 +0200

    Added about box

---
+++ app/AdminModule/presenters/AboutPresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+/**
+ * Description of AboutPresenter
+ *
+ * @author JDC
+ */
+class AboutPresenter extends BasePresenter {
+	//put your code here
+}
